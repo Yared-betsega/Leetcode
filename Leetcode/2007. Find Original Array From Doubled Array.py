73cee087d3e96727e4994e2dd5a1c78be20e2026
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-01-06 16:05:03 +0300

    Update 2007. Find Original Array From Doubled Array.py

--- Leetcode/2007. Find Original Array From Doubled Array.py
+++ Leetcode/2007. Find Original Array From Doubled Array.py
@@ -1,24 +1,25 @@
-changed = [1,2,3,2,4,6,2,4,6,4,8,12]
-if changed == [0]*len(changed):
-    print([0]*(len(changed)//2))
-changed = sorted(changed, reverse=True)
-ans = []
-for i in changed:
-    if i not in ans:
-        if i %2==0:
-            ans.append(i//2)
-        else:
-            ans.append(i)
-newArr = []
-for i in range(len(ans)):
-    newArr.append(ans[i]*2)
-for i in ans:
-    newArr.append(i)
-print(ans)
-print(newArr)
-print(changed)
-print(sorted(newArr, reverse=True))
-if sorted(newArr, reverse=True) != changed:
-    print([])
-else:
-    print(ans)
+def findOriginalArray(changed):
+        if len(changed) % 2 != 0:
+            return []
+        changed = sorted(changed)
+        original = []
+        d = {}
+        for i in changed:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        i = 0
+        while i< len(changed):
+            if d[changed[i]] > 0:
+                try:
+                    original.append(changed[i])
+                    d[changed[i]] -= 1
+                    if d[changed[i]*2] > 0:
+                        d[changed[i]*2] -= 1
+                    else:
+                        return []
+                except KeyError:
+                    return []
+            i += 1
+        return original
