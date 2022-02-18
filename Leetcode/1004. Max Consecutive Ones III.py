91e812ef8eaf532e7badd450ba485747f3c7f877
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-02-19 00:55:38 +0300

    Create 1004. Max Consecutive Ones III.py

---
+++ Leetcode/1004. Max Consecutive Ones III.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        fast = slow = 0
+        zeros = 0
+        maxim = 0
+        while fast < len(nums):
+            if nums[fast] == 0:
+                zeros += 1
+            if zeros == k+1:
+                if fast - slow > maxim:
+                    maxim = fast - slow
+                while nums[slow] == 1:
+                    slow += 1
+                slow += 1
+                zeros -= 1
+            if fast - slow +1 > maxim:
+                maxim = fast - slow+1
+            fast += 1        
+        return maxim
+    
+#time space complexity
+#time complexity  O(n)
+#space complexity O(1)
