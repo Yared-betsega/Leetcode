11b1eed2c501fd2be36b6b036a7924164da38699
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-02-22 17:11:40 +0300

    Create 703. Kth Largest Element in a Stream.py

---
+++ Leetcode/703. Kth Largest Element in a Stream.py
@@ -0,0 +1,20 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        heapq.heapify(nums)
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        for i in range(len(self.nums) - self.k):
+            heapq.heappop(self.nums)
+            
+        return self.nums[0]
+        
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
