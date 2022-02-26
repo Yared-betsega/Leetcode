d7fef98bf864c9502912bbc98e2a689f5e2bc010
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-02-26 16:56:24 +0300

    Create 287. Find the Duplicate Number.py

---
+++ Leetcode/287. Find the Duplicate Number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        left, right = min(nums), max(nums)
+        
+        while left < right:
+            mid = (left + right) // 2
+            sides = self.count(nums, mid, left, right)
+            if sides[0] > sides[1]:
+                right = mid
+            else:
+                left = mid + 1
+      
+        return right
+    
+    def count(self, nums, mid, left, right):
+        leftSide = rightSide = 0
+        for num in nums:
+            if num >= left and num <= right:
+                if num <= mid:
+                    leftSide += 1
+                else:
+                    rightSide += 1
+                
+        return leftSide, rightSide
+
+# time and space complexity
+# time complexity = O(nlogn), n = max(nums) - min(nums)
+# space complexity = O(1)
+        
