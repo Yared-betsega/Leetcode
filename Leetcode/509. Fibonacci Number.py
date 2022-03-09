b1c89981eb3b17d82bf1ca228ae2bb83c8b2b00b
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-03-09 18:26:43 +0300

    Update 509. Fibonacci Number.py

--- Leetcode/509. Fibonacci Number.py
+++ Leetcode/509. Fibonacci Number.py
@@ -1,6 +1,16 @@
-def fib(n):
+class Solution:
+    def fib(self, n: int, memo = {}) -> int:
         if n == 0:
             return 0
-        if n == 1 or n==2:
+        if n <= 2:
             return 1
-        return self.fib(n-1) + self.fib(n-2)
+        if n in memo:
+            return memo[n]
+        
+        value = self.fib(n-1) + self.fib(n-2)
+        memo[n] = value
+        return value
+
+# time and space complexity
+# time complexity = O(n)
+# space complexity = O(n)
