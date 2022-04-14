1820176e8e194eff42b4790e3092aca505a1620a
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-04-14 19:15:08 +0300

    Update 547. Number of Provinces.py

--- Leetcode/547. Number of Provinces.py
+++ Leetcode/547. Number of Provinces.py
@@ -1,5 +1,47 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        # Union Find Solution
+        def find_set(i):
+            if i == parent[i]:
+                return i
+            parent[i] = find_set(parent[i])
+            return parent[i]
+        
+        def union_sets(i, j):
+            a = find_set(i)
+            b = find_set(j)
+            if a!=b:
+                if size[a] >= size[b]:
+                    parent[b] = a
+                    size[a] += size[b]
+                else:
+                    parent[a] = b
+                    size[b] += size[a]
+        
+        parent, size = {}, {}
+        for i in range(len(isConnected)):
+            parent[i] = i
+            size[i] = 1    
+        
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[0])):
+                if isConnected[i][j] == 1:
+                    union_sets(i, j)
+                    
+        count = 0
+        for k, v in parent.items():
+            if k == v:
+                count += 1
+        return count
+    
+#For union find Solution              
+# time and space complexity
+# time complexity = O(n)
+# space complexity = O(n)
+        
+        
+        # DFS solution
         def markConnected(city, visited):
             visited.add(city)
             for i in range(len(isConnected)):
