dc196a17b8f2f6aae813dc7eaef9ceb71c597522
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-05-08 21:39:31 +0300

    Update 2007. Find Original Array From Doubled Array.py

--- Leetcode/2007. Find Original Array From Doubled Array.py
+++ Leetcode/2007. Find Original Array From Doubled Array.py
@@ -23,3 +23,20 @@ def findOriginalArray(changed):
                     return []
             i += 1
         return original
+
+#Greedy Apraoch
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 != 0:
+            return []
+        changed.sort()
+        ori = []
+        queue = deque([changed[0]])
+        for i in range(1,len(changed)):
+            if not queue or changed[i] < 2*queue[0]:
+                queue.append(changed[i])
+            elif changed[i] == 2*queue[0]:
+                ori.append(queue.popleft())
+            else:
+                return []
+        return ori if len(ori) == len(changed)/2 else []
