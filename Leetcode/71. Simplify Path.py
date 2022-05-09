5b0c9f160f8c915a9676113c8b4c3f3b5c9f7dac
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-05-09 19:26:33 +0300

    Update 71. Simplify Path.py

--- Leetcode/71. Simplify Path.py
+++ Leetcode/71. Simplify Path.py
@@ -3,16 +3,14 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path = path.split("/")
-        stack = ["/"]
+        stack = []
         for subDir in path:
             if subDir == "..":
-                if len(stack) > 1:
+                if stack:
                     stack.pop()
             elif subDir != "" and subDir != ".":
-                stack.append(subDir + "/")  
-                
-        answer = "".join(stack)
-        return answer[:-1] if len(answer)>1 and answer[-1] == "/" else answer
+                stack.append(subDir)    
+        return "/" + "/".join(stack)
 
 # time complexity = O(n)
 # space complexity = O(n)
