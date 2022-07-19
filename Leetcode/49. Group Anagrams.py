57cc39cb7d2faa776726274828fd7c4dcab254be
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-19 23:02:23 +0300

    Create 49. Group Anagrams.py

---
+++ Leetcode/49. Group Anagrams.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = defaultdict(list)
+        for st in strs:
+            group[tuple(sorted(st))].append(st)
+        return group.values()
+
+# time and space complexity
+# n = len(strs)
+# m = len(strs[i])
+# time: O(nmlog(m))
+# space: O(n)
