b3b57d1b46624944be3199959ae8b119de6d8dd7
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-20 00:26:41 +0300

    Time: 57 ms (51.59%), Space: 14 MB (5.43%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,10 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(len(nums2)):
-            nums1.pop()
-        for i in range(len(nums2)):
-            nums1.append(nums2[i])
-        nums1.sort()
-            
         
+        # # Solution using sorting
+        # for i in range(len(nums2)):
+        #     nums1.pop()
+        # for i in range(len(nums2)):
+        #     nums1.append(nums2[i])
+        # nums1.sort()
+        
+        # O(m+n) Solution or optimum solution
+        nums1_last, nums2_last = m-1, n-1
+        if n != 0:
+            for i in range(m + n - 1, -1, -1):
+                if nums1[nums1_last] >= nums2[nums2_last]:
+                    nums1[i] = nums1[nums1_last]
+                    nums1_last -= 1
+                elif nums1[nums1_last] < nums2[nums2_last]:
+                    nums1[i] = nums2[nums2_last]
+                    nums2_last -= 1
+                if nums1_last < 0 or nums2_last < 0:
+                    break  
+
+            if nums1_last < 0:
+                for i in range(nums2_last, -1, -1):
+                    nums1[i] = nums2[i]  
