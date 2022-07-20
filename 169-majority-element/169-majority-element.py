e4718478942950901183c04efdefa0cbe4a18a8f
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-20 15:25:34 +0300

    Time: 308 ms (24.26%), Space: 15.5 MB (34.60%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,8 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        maxim = -1
-        for elem in count:
-            if count[elem] > len(nums)/2:
-                return elem
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+            if count[num] > len(nums)/2:
+                return num
+            
+        # count = Counter(nums)
+        # maxim = -1
+        # for elem in count:
+        #     if count[elem] > len(nums)/2:
+        #         return elem
             
