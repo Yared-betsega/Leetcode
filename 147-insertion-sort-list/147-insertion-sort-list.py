60029a3fda95487feb4a9370d48aea8460bad788
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-21 21:19:45 +0300

    Time: 1535 ms (48.87%), Space: 16.7 MB (22.43%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.py
+++ 147-insertion-sort-list/147-insertion-sort-list.py
@@ -11,20 +11,20 @@ class Solution:
         while node:
             prevChecker = ListNode(0, head)
             checker = head
-            found = False
+            position_updated = False
             for j in range(i):
                 if checker.val > node.val:
                     prev.next = node.next
                     node.next = checker
                     prevChecker.next = node
                     node = prev.next
-                    found = True
                     if j == 0:
                         head = prevChecker.next
+                    position_updated = True
                     break
                 prevChecker = prevChecker.next
                 checker = checker.next 
-            if not found:
+            if not position_updated:
                 prev = node
                 node = node.next if node else None
             i += 1
