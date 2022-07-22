150753268298f181b1e096a896fd43cb5ab810e7
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 23:27:17 +0300

    Time: 1243 ms (51.99%), Space: 21.2 MB (30.73%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -7,16 +7,12 @@ class Solution:
                 nums[i] = 1
         
         prefix_sum_frequency = defaultdict(int)
-        prefixSum = [nums[0]]
-        prefix_sum_frequency[prefixSum[0]] += 1
+        prefixSum = 0
+        prefix_sum_frequency[prefixSum] += 1
         ans = 0
-        if prefixSum[0] == k: # If the first element equals k
-                ans += 1
-        for i in range(1, len(nums)):
-            prefixSum.append(nums[i] + prefixSum[i-1]) 
-            if prefixSum[-1] == k:
-                ans += 1
-            ans += prefix_sum_frequency[prefixSum[-1] - k]
-            prefix_sum_frequency[prefixSum[-1]] += 1
-            
+        for i in range(len(nums)):
+            prefixSum += nums[i]
+            ans += prefix_sum_frequency[prefixSum - k]
+            prefix_sum_frequency[prefixSum] += 1 
+
         return ans
