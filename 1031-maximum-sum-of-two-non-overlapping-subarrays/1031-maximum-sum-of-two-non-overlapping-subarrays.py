2dd4f902d1db85124d137f4e1a6ef9a935b6ea9e
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 21:43:10 +0300

    Time: 765 ms (11.14%), Space: 14.1 MB (25.75%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,52 +1,34 @@
-# https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
-
-
-
 class Solution:
     def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
-        overlap = lambda f, s: s[0] <= f[0] <= s[1] or s[0] <= f[1] <= s[1] or f[0] <= s[0] <= f[1] or f[0] <= s[1] <= f[1]
-        
-        first = []
-        second = []
-        prefix = [0] * len(nums)
-        
-        prefix[0] = nums[0]
-        
-        for i in range(1, len(nums)):
-            prefix[i] = prefix[i-1] + nums[i]
-        
-        first_len_max = []
-        second_len_max = []
-        
-        for i in range(firstLen - 1, len(nums)):
-            start = -1
-            initial = 0
-            if (i - firstLen) > -1:
-                initial = prefix[i-firstLen]
-                start = i - firstLen
-            first_len_max.append((prefix[i] - initial, (start+1, i)))
+        maxSum = 0
+        left_first, right_first = 0, firstLen-1
+        firstSum = sum(nums[left_first:right_first + 1])
+        while right_first < len(nums):
+            left_second, right_second = 0, secondLen-1
+            secondSum = sum(nums[left_second: right_second+1])
+            while right_second < left_first:
+                maxSum = max(firstSum + secondSum, maxSum)
+                secondSum -= nums[left_second]
+                left_second += 1
+                right_second += 1
+                secondSum += nums[right_second]
+            left_second, right_second = right_first + 1, right_first + secondLen
+            secondSum = sum(nums[left_second: right_second + 1])
+            while right_second < len(nums):
+                maxSum = max(firstSum + secondSum, maxSum)
+                secondSum -= nums[left_second]
+                left_second += 1
+                right_second += 1
+                secondSum += nums[right_second] if right_second < len(nums) else 0
+            firstSum -= nums[left_first]
+            left_first += 1
+            right_first += 1
+            firstSum += nums[right_first] if right_first < len(nums) else 0
+        return maxSum
+
+# time and space complexity
+# time: O(n**2)
+# space: O(n)
+
             
-        
-        for j in range(secondLen - 1, len(nums)):
-            start = -1
-            initial = 0
-            if j - secondLen >= 0:
-                initial = prefix[j-secondLen]
-                start = j - secondLen
-            second_len_max.append((prefix[j] - initial, (start+1, j)))
-            
-        first_len_max.sort(key=lambda x: x[0])
-        second_len_max.sort(key=lambda x: x[0])
-            
-        choice = -1
-        for j in range(len(second_len_max)-1, -1, -1):
-            for i in range(len(first_len_max)-1, -1, -1):
-                if not overlap(first_len_max[i][1], second_len_max[j][1]):
-#                     if first_len_max[i][0] + second_len_max[j][0] == 108:
-#                         print("first: ", first_len_max[i][1], "second: ", second_len_max[j][1])
-                    choice = max(first_len_max[i][0] + second_len_max[j][0], choice)
-                    break
                 
-        return choice
-         
-        
