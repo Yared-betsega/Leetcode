cbe53d1b48154cdc15219793e82302775c032ba8
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 14:44:37 +0300

    Time: 1868 ms (71.61%), Space: 31 MB (90.24%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -2,39 +2,39 @@ class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         if not nums:
             return nums
-        ##Space O(n) solution
-        # lessThan, moreThan, equals = [], [], []
-        # for num in nums:
-        #     if num < pivot:
-        #         lessThan.append(num)
-        #     elif num == pivot:
-        #         equals.append(num)
-        #     else:
-        #         moreThan.append(num)
-        # lessThan.extend(equals)
-        # lessThan.extend(moreThan)
-        # return lessThan 
+        #Space O(n) solution
+        lessThan, moreThan, equals = [], [], []
+        for num in nums:
+            if num < pivot:
+                lessThan.append(num)
+            elif num == pivot:
+                equals.append(num)
+            else:
+                moreThan.append(num)
+        lessThan.extend(equals)
+        lessThan.extend(moreThan)
+        return lessThan 
         
 # time and space complexity
 # time: O(n)
 # space: O(n)
 
-        equals = math.inf
-        for i in range(len(nums)):
-            if nums[i] > pivot:
-                nums.append(nums[i])
-                nums[i] = "0"
-            elif nums[i] < pivot:
-                if equals < i:
-                    nums[equals], nums[i] = nums[i], nums[equals]
-                    while nums[equals] != pivot:
-                        equals += 1
-            else:
-                if equals == math.inf:
-                    equals = i
+#         equals = math.inf
+#         for i in range(len(nums)):
+#             if nums[i] > pivot:
+#                 nums.append(nums[i])
+#                 nums[i] = "0"
+#             elif nums[i] < pivot:
+#                 if equals < i:
+#                     nums[equals], nums[i] = nums[i], nums[equals]
+#                     while nums[equals] != pivot:
+#                         equals += 1
+#             else:
+#                 if equals == math.inf:
+#                     equals = i
                 
-        return list(filter(lambda x: x != "0", nums))
+#         return list(filter(lambda x: x != "0", nums))
 
-# time and space complexity
-# time: O(n)
-# space: O(1)
+# # time and space complexity
+# # time: O(n)
+# # space: O(1)
