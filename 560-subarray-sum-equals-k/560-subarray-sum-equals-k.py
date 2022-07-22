e1c13b1cf6994691e195f169ef5a1e12ec1f7c9d
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 23:01:31 +0300

    Time: 442 ms (49.54%), Space: 18.6 MB (7.94%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,17 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prev = defaultdict(int)
+        prefix_sum_frequency = defaultdict(int)
         prefixSum = [nums[0]]
-        prev[prefixSum[0]] += 1
+        prefix_sum_frequency[prefixSum[0]] += 1
         ans = 0
-        if prefixSum[0] == k:
+        if prefixSum[0] == k: # If the first element equals k
                 ans += 1
         for i in range(1, len(nums)):
             prefixSum.append(nums[i] + prefixSum[i-1]) 
             if prefixSum[-1] == k:
                 ans += 1
-            ans += prev[prefixSum[-1] - k]
-            prev[prefixSum[-1]] += 1
+            ans += prefix_sum_frequency[prefixSum[-1] - k]
+            prefix_sum_frequency[prefixSum[-1]] += 1
             
         return ans
 
