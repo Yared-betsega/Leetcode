8f1bea8c6ccd19db599b50ad2897c3a9078c42a9
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 14:42:10 +0300

    Time: 2866 ms (10.23%), Space: 31.6 MB (44.00%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -2,18 +2,33 @@ class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         if not nums:
             return nums
-        lessThan, moreThan, equals = [], [], []
-        for num in nums:
-            if num < pivot:
-                lessThan.append(num)
-            elif num == pivot:
-                equals.append(num)
+        # lessThan, moreThan, equals = [], [], []
+        # for num in nums:
+        #     if num < pivot:
+        #         lessThan.append(num)
+        #     elif num == pivot:
+        #         equals.append(num)
+        #     else:
+        #         moreThan.append(num)
+        # lessThan.extend(equals)
+        # lessThan.extend(moreThan)
+        # return lessThan 
+        
+        equals = math.inf
+        for i in range(len(nums)):
+            if nums[i] > pivot:
+                nums.append(nums[i])
+                nums[i] = "0"
+            elif nums[i] < pivot:
+                if equals < i:
+                    nums[equals], nums[i] = nums[i], nums[equals]
+                    while nums[equals] != pivot:
+                        equals += 1
             else:
-                moreThan.append(num)
-        lessThan.extend(equals)
-        lessThan.extend(moreThan)
-        return lessThan 
-    
+                if equals == math.inf:
+                    equals = i
+                
+        return list(filter(lambda x: x != "0", nums))
 # time and space complexity
 # time: O(n)
-# space: O(1)
+# space: O(n)
