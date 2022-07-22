2c6b2f6d14e3d969de7ba5ce560378a97c1b9deb
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-22 21:35:53 +0300

    Time: 427 ms (20.56%), Space: 14 MB (89.33%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,24 +1,29 @@
 class Solution:
     def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
         maxSum = 0
-        left_first, right_first = 0, firstLen
-        while right_first <= len(nums):
-            firstSum = sum(nums[left_first:right_first])
-            left_second, right_second = 0, secondLen
-            while right_second <= left_first:
-                secondSum = sum(nums[left_second: right_second])
+        left_first, right_first = 0, firstLen-1
+        firstSum = sum(nums[left_first:right_first + 1])
+        while right_first < len(nums):
+            left_second, right_second = 0, secondLen-1
+            secondSum = sum(nums[left_second: right_second+1])
+            while right_second < left_first:
                 maxSum = max(firstSum + secondSum, maxSum)
+                secondSum -= nums[left_second]
                 left_second += 1
                 right_second += 1
-            left_second, right_second = right_first, right_first + secondLen
-            while right_second <= len(nums):
-                secondSum = sum(nums[left_second: right_second])
+                secondSum += nums[right_second]
+            left_second, right_second = right_first + 1, right_first + secondLen
+            secondSum = sum(nums[left_second: right_second + 1])
+            while right_second < len(nums):
                 maxSum = max(firstSum + secondSum, maxSum)
+                secondSum -= nums[left_second]
                 left_second += 1
                 right_second += 1
+                secondSum += nums[right_second] if right_second < len(nums) else 0
+            firstSum -= nums[left_first]
             left_first += 1
             right_first += 1
-            
+            firstSum += nums[right_first] if right_first < len(nums) else 0
         return maxSum
 
             
