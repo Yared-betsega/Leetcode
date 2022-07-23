f113bd9ca164e5b3f7fe4945e1c01436f11a5f97
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-23 21:23:08 +0300

    Time: 4546 ms (46.67%), Space: 32.9 MB (73.36%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        def binary_search(ls, value):
+            left, right = 0, len(ls) - 1
+            while left < right:
+                mid = (left + right) // 2
+                if ls[mid] >= value:
+                    right = mid
+                elif ls[mid] < value:
+                    left = mid + 1
+            return right
+                
+        sorted_nums = sorted(nums)
+        counts = []
+        for i in range(len(nums)):
+            # print(nums[i])
+            pos = binary_search(sorted_nums, nums[i])
+            # print(sorted_nums[pos-1])
+            counts.append(pos)
+            del sorted_nums[pos]
+        return counts
