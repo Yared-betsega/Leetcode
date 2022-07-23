960e2655159ec5256076265404a1e5574b2b7622
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-23 22:43:25 +0300

    Time: 78 ms (6.53%), Space: 13.8 MB (55.35%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,10 +1,12 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        prefix_sum = [gain[0]]
+        prefix_sum = gain[0]
+        ans = prefix_sum
         for i in range(1, len(gain)):
-            prefix_sum.append(prefix_sum[i-1] + gain[i])
-        return max(prefix_sum + [0])
+            prefix_sum = prefix_sum + gain[i]
+            ans = max(ans, prefix_sum)
+        return max(0, ans)
 
 # time and space complexity
 # time: O(n)
-# space: O(n)
+# space: O(1)
