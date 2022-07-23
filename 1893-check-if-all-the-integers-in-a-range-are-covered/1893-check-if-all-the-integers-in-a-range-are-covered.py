0b945c4fc82d0e78bfefcf7ace467ecc713fdfd3
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-23 22:52:46 +0300

    Time: 74 ms (27.85%), Space: 13.9 MB (30.22%) - LeetHub

---
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        for i in range(left, right + 1):
+            notCovered = True
+            for j in ranges:
+                if i >= j[0] and i <= j[1]:
+                    notCovered = False
+                    break
+            if notCovered:
+                return False
+        
+        return True
+            
+                
