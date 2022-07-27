dfe8ba467bb6be05ca690c3a1e06ef8f9975ee9a
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-27 14:48:25 +0300

    Time: 47 ms (71.68%), Space: 14.6 MB (92.36%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -9,28 +9,22 @@ class Node:
 """
 
 class Solution:
-    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        def construct(node):
-            while node:
-                if node.child:
-                    child = node.child
-                    while child.next:
-                        child = child.next
-                    child.next = node.next
-                    node.next = child
-                    node = child
-            
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':    
         node = head
         while node:
-            if node.child:
+            if node.child: # If there is a child travel to last node of the child
                 child = node.child
                 while child.next:
                     child = child.next
-                child.next = node.next
-                if node.next:
+                child.next = node.next # Update the next of child to the the next of the current node
+                if node.next: # update the prev of the next node to chile to make it valid doubly linked list
                     node.next.prev = child
-                node.next = node.child
-                node.next.prev = node
-                node.child = None
+                node.next = node.child # Update the child to become the next of the current
+                node.next.prev = node # update the prev of the next node to chile to make it valid doubly linked list
+                node.child = None # Make the child of the current node None to fulfill the requirements
             node = node.next
         return head
+
+# time and space complexity
+# time: O(n)
+# space: O(1)
