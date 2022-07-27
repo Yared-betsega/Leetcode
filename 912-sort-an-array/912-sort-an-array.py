410eb6395485b6ed4f4e792d365d164afccbefcb
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-27 22:27:47 +0300

    Time: 1314 ms (22.12%), Space: 22.4 MB (35.20%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def mergeSort(array):
+            if len(array) == 1:
+                return array
+            
+            mid = len(array) // 2
+            left = mergeSort(array[:mid])
+            right = mergeSort(array[mid:])
+            l =  r = 0
+            temp = []
+            for i in range(len(array)):
+                if l == len(left):
+                    temp += right[r:]
+                    break
+                elif r == len(right):
+                    temp += left[l:]
+                    break
+                else:
+                    if left[l] <= right[r]:
+                        temp.append(left[l])
+                        l += 1
+                    else:
+                        temp.append(right[r])
+                        r += 1
+            return temp
+        
+        return mergeSort(nums)
+                                
+                    
