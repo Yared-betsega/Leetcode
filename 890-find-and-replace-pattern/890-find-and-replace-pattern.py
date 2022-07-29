13c61235b6e44e6d9775c3b81da34fa9cfcb7dba
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-29 20:32:40 +0300

    Time: 37 ms (87.02%), Space: 13.8 MB (76.64%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        answer = []
+        for word in words:
+            mapping, taken, valid = {}, set(), True
+            for i in range(len(word)):
+                if pattern[i] in mapping:
+                    if mapping[pattern[i]] != word[i]:
+                        valid = False
+                        break
+                else:
+                    if word[i] in taken:
+                        valid = False 
+                        break
+                    mapping[pattern[i]] = word[i]
+                    taken.add(word[i])
+            if valid:
+                answer.append(word)
+        return answer
+                
