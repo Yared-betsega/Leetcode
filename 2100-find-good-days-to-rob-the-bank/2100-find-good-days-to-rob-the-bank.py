a0b08e6938c9b599ae78ae541b688e7bd4e4486a
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-07-31 23:56:56 +0300

    Time: 1805 ms (28.47%), Space: 41 MB (5.02%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -12,9 +12,14 @@ class Solution:
             else:
                 inc = 1
             monotonicity[i] = [dec, inc]
+            
         good_days = []
         for i in range(len(security)):
             if i - time >= 0 and i + time <= len(security) - 1:
-                if monotonicity[i][0] >= time+1 and monotonicity[i+time][1] >= time+1:
+                if monotonicity[i][0] >= time + 1 and monotonicity[i + time][1] >= time + 1:
                     good_days.append(i)
         return good_days
+
+# time and space complexity
+# time: O(n)
+# space: O(n)
