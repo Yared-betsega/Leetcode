3c8682db56108cc9e3e09c9e10c1137edf79c442
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-03 04:31:31 +0300

    Time: 61 ms (93.93%), Space: 14.1 MB (20.20%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,6 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-
+#         # DP top down implementation
+#         def climb(idx, total, memo):
+#             if idx > len(cost) - 1:
+#                 return total
+#             if (idx,total) in memo:
+#                 return memo[(idx, total)]
+             
+#             value = cost[idx] + min(climb(idx+1, total, memo), climb(idx+2, total, memo))
+#             memo[(idx, total)] = value
+#             return value
+        
+#         return min(climb(0, 0, defaultdict(list)), climb(1, 0, defaultdict(list)))
+#         # time and space complexity
+#         # time: O(n)
+#         # space: O(1)
         
         # DP bottom up implementation
         dp = [cost[i] for i in range(len(cost))]
