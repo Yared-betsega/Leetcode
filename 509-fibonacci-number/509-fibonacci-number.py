11a28777fbb0e1a71b360bc9e9ba027fbd5b740e
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-03 21:34:33 +0300

    Time: 54 ms (48.43%), Space: 13.7 MB (95.62%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    # top down implemention
+    def fib(self, n: int, memo = {}) -> int:
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        if n in memo:
+            return memo[n]
+        
+        value = self.fib(n-1) + self.fib(n-2)
+        memo[n] = value
+        return value
+
+# time and space complexity
+# time complexity = O(n)
+# space complexity = O(n)
