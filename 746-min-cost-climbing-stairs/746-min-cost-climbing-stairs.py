d2c6c6c33deabb50de3baa32e69ab8eccd7e2a5c
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-03 04:30:42 +0300

    Time: 94 ms (50.27%), Space: 14 MB (41.46%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
+#         # DP top down implementation
 #         def climb(idx, total, memo):
 #             if idx > len(cost) - 1:
 #                 return total
@@ -12,7 +12,11 @@ class Solution:
 #             return value
         
 #         return min(climb(0, 0, defaultdict(list)), climb(1, 0, defaultdict(list)))
+#         # time and space complexity
+#         # time: O(n)
+#         # space: O(1)
         
+        # DP bottom up implementation
         dp = [cost[i] for i in range(len(cost))]
         plusOne = cost[-2]
         plusTwo = cost[-1]
@@ -21,3 +25,7 @@ class Solution:
             plusTwo = plusOne
             plusOne = cur
         return min(plusOne, plusTwo)
+        
+        # time and space complexity
+        # time: O(n)
+        # space: O(1)
