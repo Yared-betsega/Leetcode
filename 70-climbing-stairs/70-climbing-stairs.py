aa27df1ddc5b21eccb25a30049562ca8ffd1a4e2
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-05 16:15:04 +0300

    Time: 63 ms (10.27%), Space: 13.9 MB (11.90%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,19 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # DP bottom up implementation
         dp = defaultdict(int)
-        oneStep = 1
-        twoStep = 1
-        
+        oneStep, twoStep = 1, 1
         if n < 2:
             return 1
-        
         for i in range(2, n+1):
             temp = oneStep + twoStep
             oneStep = twoStep
             twoStep = temp
-        
+            
         return twoStep
 
 # time and space complexity
 # time complexity = O(n)
-# space complexity =O(n)
+# space complexity =O(1)
