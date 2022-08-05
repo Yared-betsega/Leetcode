3c3126435b243a7e518f013f1d997cbc869b5931
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-05 21:14:03 +0300

    Time: 72 ms (34.79%), Space: 14.3 MB (10.06%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,13 +1,17 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        @cache
+        memo = {}
         def helper(target):
             if target < 0:
                 return 0
             if target == 0:
                 return 1
+            if target in memo:
+                return memo[target]
             ans = 0
             for num in nums:
                 ans += helper(target - num)
-            return ans
+            memo[target] = ans
+            return memo[target]
+        
         return helper(target)
