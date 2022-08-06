8823926b61c28f45868d6324dff5ce859f852657
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-06 16:49:14 +0300

    Time: 27 ms (98.54%), Space: 14 MB (20.44%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.py
+++ 458-poor-pigs/458-poor-pigs.py
@@ -1,5 +1,4 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        trials = minutesToTest // minutesToDie + 1
-        return ceil(log(buckets, trials))
+        return ceil(log(buckets, minutesToTest // minutesToDie + 1))
         
