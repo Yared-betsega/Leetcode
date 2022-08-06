3cbb499578fcd24856e59c7e699fe05146c5f3f9
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-06 16:55:10 +0300

    Time: 30 ms (94.89%), Space: 13.7 MB (99.27%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.py
+++ 458-poor-pigs/458-poor-pigs.py
@@ -1,4 +1,7 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         return ceil(log(buckets, minutesToTest // minutesToDie + 1))
-        
+    
+# time and space complexity
+# time: O(1)
+# space: O(1)
