aaa827d57d534ed43e41ed8855f5bd98e74855c4
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 20:52:06 +0300

    Time: 44 ms (65.84%), Space: 14.1 MB (19.58%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        dp = nums[:]
-        
-        for i in range(len(nums)):
-            for j in range(i-1):
-                dp[i] = max( nums[i] + dp[j], dp[i] )
-            
-        return max(dp)
+        @cache
+        def choose(i):
+            if i >= len(nums):
+                return 0
+            return max(choose(i + 1), nums[i] + choose(i + 2))
+        return choose(0)
 
 # time and space complexity
 # time complexity = O(n ** 2)
