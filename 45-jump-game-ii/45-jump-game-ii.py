d0b009b6a667c5eee46ec46f5db2dace20606853
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 23:12:31 +0300

    Time: 188 ms (72.54%), Space: 15.1 MB (23.18%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        
+        # Single loop O(n) Solution
         if len(nums) == 1:
             return 0
         maxReach, steps = nums[0], 1
@@ -18,26 +20,27 @@ class Solution:
                 
             
         
+        # Dynamic Programming Top Down Approach
+        @cache
+        def chooseIndex(i):
+            if i >= len(nums) - 1:
+                return 0
+            ans = float("inf")
+            for j in range(i + 1, i + nums[i] + 1):
+                ans = min(ans, chooseIndex(j) + 1)
+            return ans
+        return chooseIndex(0)
         
-        # @cache
-        # def chooseIndex(i):
-        #     if i >= len(nums) - 1:
-        #         return 0
-        #     ans = float("inf")
-        #     for j in range(i + 1, i + nums[i] + 1):
-        #         ans = min(ans, chooseIndex(j) + 1)
-        #     return ans
-        # return chooseIndex(0)
-        
-#         if len(nums) == 1:
-#             return 0
-#         dp = [float("inf")] * len(nums)
-#         dp[0] = 0
-#         for i in range(len(nums)):
-#             for j in range(i + 1, min(len(nums), i + nums[i] + 1)):
-#                 dp[j] = min(dp[j], dp[i] + 1)
+        # Dynamic Programming Bottom Up Approach
+        if len(nums) == 1:
+            return 0
+        dp = [float("inf")] * len(nums)
+        dp[0] = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, min(len(nums), i + nums[i] + 1)):
+                dp[j] = min(dp[j], dp[i] + 1)
             
                 
-#         return dp[-1]
+        return dp[-1]
 
             
