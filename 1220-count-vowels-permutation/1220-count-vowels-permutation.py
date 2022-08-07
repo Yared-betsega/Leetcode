0ea590d86cbb036da79e14f0fa046a0cd910c481
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 09:47:49 +0300

    Time: 1963 ms (7.35%), Space: 171 MB (19.89%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -7,14 +7,18 @@ class Solution:
             "o": ["i", "u"],
             "u": ["a"]
         }
-        @lru_cache
+        memo = {}
         def dfs(letter, count):
             if count == n:
                 return 1
+            if (letter, count) in memo:
+                return memo[(letter, count)]
             ans = 0
             for item in follower[letter]:
                 ans += dfs(item, count + 1)
-            return ans
+            memo[(letter, count)] = ans
+            return memo[(letter, count)]
+        
         ans = 0
         for letter in follower:
             ans += dfs(letter, 1)
