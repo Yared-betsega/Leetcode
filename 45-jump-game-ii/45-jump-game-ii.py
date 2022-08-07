06b2af51b2945166d6af6f4eb24d5fed46d71e73
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 22:24:00 +0300

    Time: 8357 ms (19.30%), Space: 15 MB (59.20%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,11 +1,29 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        @cache
-        def chooseIndex(i):
-            if i >= len(nums) - 1:
-                return 0
-            ans = float("inf")
-            for j in range(i + 1, i + nums[i] + 1):
-                ans = min(ans, chooseIndex(j) + 1)
-            return ans
-        return chooseIndex(0)
+        # maxReach, steps = nums[0], {}
+        # for i in range(len(nums)):
+        #     if 
+        
+        
+        # @cache
+        # def chooseIndex(i):
+        #     if i >= len(nums) - 1:
+        #         return 0
+        #     ans = float("inf")
+        #     for j in range(i + 1, i + nums[i] + 1):
+        #         ans = min(ans, chooseIndex(j) + 1)
+        #     return ans
+        # return chooseIndex(0)
+        
+        if len(nums) == 1:
+            return 0
+        dp = [float("inf")] * len(nums)
+        dp[0] = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, min(len(nums), i + nums[i] + 1)):
+                dp[j] = min(dp[j], dp[i] + 1)
+            
+                
+        return dp[-1]
+
+            
