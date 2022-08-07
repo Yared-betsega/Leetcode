2a6d3168f34456c70eb4b471ae123f039f6ad22f
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 10:18:59 +0300

    Time: 1205 ms (27.06%), Space: 43.5 MB (42.72%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
+        # First build the connection
+        # For the case of I, since it cannot be followed by the itself, it may be followed by the other vowels
         follower = {
             "a": ["e"],
             "e": ["a", "i"],
@@ -7,21 +10,29 @@ class Solution:
             "o": ["i", "u"],
             "u": ["a"]
         }
-        memo = {}
-        def dfs(letter, count):
+        
+        # Accepts the current letter and 
+        # the length of the string formed till now
+        
+        @lru_cache
+        def dfs(letter, count): 
             if count == n:
                 return 1
-            if (letter, count) in memo:
-                return memo[(letter, count)]
-            ans = 0
+            permutation = 0
             for item in follower[letter]:
-                ans += dfs(item, count + 1)
-            memo[(letter, count)] = ans
-            return memo[(letter, count)]
+                # Add the result of adding the next possible follower of letter
+                permutation += dfs(item, count + 1)
+            
+            return permutation
         
         ans = 0
+        # Iterate over the starting five vowels 
+        # We can avoid this two lines of code by adding "": ["a", "e", "i", "o", "u"] on our hashmap
         for letter in follower:
-            ans += dfs(letter, 1)
+            ans += dfs(letter, 1) # Add the result of starting from each vowel
             
         return ans % (10**9 + 7)
-            
+
+# time and space complexity
+# time: O(n)
+# space: O(n)            
