b898cb29da5e3b9b43c1a8b43fc99850f50bfd88
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 22:33:02 +0300

    Time: 8029 ms (5.00%), Space: 40.8 MB (5.04%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,15 +1,25 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        dp = [False] * len(nums)
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] == 0:
-                dp[i]  = False
-            elif i + nums[i] >= len(nums) - 1:
-                dp[i] = True
-            else:
-                if any(dp[i:i + nums[i] + 1]):
-                    dp[i] = True
+#         if len(nums) == 1:
+#             return True
+#         dp = [False] * len(nums)
+#         for i in range(len(nums) - 2, -1, -1):
+#             if nums[i] == 0:
+#                 dp[i]  = False
+#             elif i + nums[i] >= len(nums) - 1:
+#                 dp[i] = True
+#             else:
+#                 if any(dp[i:i + nums[i] + 1]):
+#                     dp[i] = True
                 
-        return dp[0]
+#         return dp[0]
+        @cache
+        def chooseIndex(i):
+            if i + nums[i] >= len(nums) - 1:
+                return True
+            for j in range(i + 1, i + nums[i] + 1):
+                if chooseIndex(j):
+                    return True
+            return False
+        return chooseIndex(0)
+
