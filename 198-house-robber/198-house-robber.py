867d17c1faf785716c2629998d45aefbc2e07987
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-07 21:05:43 +0300

    Time: 52 ms (44.13%), Space: 14 MB (19.58%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -6,6 +6,8 @@ class Solution:
                 return 0
             return max(choose(i + 1), nums[i] + choose(i + 2))
         return choose(0)
+        
+        
 
 # time and space complexity
 # time complexity = O(n ** 2)
