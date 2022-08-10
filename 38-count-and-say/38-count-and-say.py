b34d386b17b660b7925c7a8b48cb7520983aeb02
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-10 10:19:27 +0300

    Time: 59 ms (75.32%), Space: 13.9 MB (53.75%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -3,15 +3,12 @@ class Solution:
         if n == 1:
              return "1"
         num = self.countAndSay(n-1)
-        cur = 0
-        ans = ""
+        cur, ans = 0, ""
         for i in range(len(num)):
             if num[i] != num[cur]:
-                ans += str(i-cur)
-                ans += str(num[cur])
+                ans += str(i-cur) + str(num[cur])
                 cur = i
             if i == len(num) - 1:
-                ans += str(i-cur+1)
-                ans += str(num[cur])
+                ans += str(i-cur+1) + str(num[cur])
                 cur = i
-        return ans            
+        return ans  
