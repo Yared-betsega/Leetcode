47e64d0e2d94689de11c958e5f94dc3e0d5f97e3
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-12 22:39:26 +0300

    Time: 665 ms (74.56%), Space: 19 MB (53.30%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -1,24 +1,43 @@
+# class Solution:
+#     def maxSubarraySumCircular(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         nums *= 2
+#         prefix_sum = [nums[0]]
+#         for i in range(1, len(nums)):
+#             prefix_sum.append(prefix_sum[-1] + nums[i])
+        
+#         queue = deque([0])
+#         ans = nums[0]
+#         for i in range(1, len(prefix_sum)):
+#             if queue[0] < i - n:
+#                 queue.popleft()
+            
+#             ans = max(ans, prefix_sum[i] - prefix_sum[queue[0]])
+            
+#             while queue and prefix_sum[i] <= prefix_sum[queue[-1]]:
+#                 queue.pop()
+#             queue.append(i)
+#         return ans
+
+# # time and space complexity
+# # time: O(n)
+# # space: O(n)
+
+
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        n = len(nums)
-        nums *= 2
-        prefix_sum = [nums[0]]
+        max_dp, min_dp = nums[:], nums[:]
         for i in range(1, len(nums)):
-            prefix_sum.append(prefix_sum[-1] + nums[i])
-        
-        queue = deque([0])
-        ans = nums[0]
-        for i in range(1, len(prefix_sum)):
-            if queue[0] < i - n:
-                queue.popleft()
-            
-            ans = max(ans, prefix_sum[i] - prefix_sum[queue[0]])
-            
-            while queue and prefix_sum[i] <= prefix_sum[queue[-1]]:
-                queue.pop()
-            queue.append(i)
-        return ans
-
+            max_dp[i] = max(nums[i], nums[i] + max_dp[i-1])
+            min_dp[i] = min(nums[i], nums[i] + min_dp[i-1])
+        s = sum(nums)
+        _min = min(min_dp)
+        _max = max(max_dp)
+        if s == _min:
+            return _max
+        return max(_max, s - _min)
+    
+    
 # time and space complexity
 # time: O(n)
 # space: O(n)
