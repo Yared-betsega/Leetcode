bbaa5c3ebaa43f57132e0213ea42b6ab84e2af72
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-13 17:58:40 +0300

    Time: 796 ms (44.85%), Space: 14.3 MB (38.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -3,22 +3,27 @@ class Solution:
         len_of_word_in_words = len(words[0])
         concatination_length = len(words) * len_of_word_in_words
         count = Counter(words)
+        concatinations = set()
         i, ans = 0, []
         while i + concatination_length <= len(s):
-            added = count.copy()
-            c, st, isConcatination = 1, "", True
-            for j in range(i, i + concatination_length):
-                st += s[j]
-                if c % len_of_word_in_words == 0:
-                    if st in added and added[st] > 0:
-                        added[st] -= 1
-                        st = ""
-                    else:
-                        isConcatination = False
-                        break
-                c += 1
+            isConcatination = True
+            candidate = s[i: i + concatination_length]
+            if candidate not in concatinations:
+                added = count.copy()
+                c, st  = 1, ""
+                for j in range(i, i + concatination_length):
+                    st += s[j]
+                    if c % len_of_word_in_words == 0:
+                        if st in added and added[st] > 0:
+                            added[st] -= 1
+                            st = ""
+                        else:
+                            isConcatination = False
+                            break
+                    c += 1
             if isConcatination:
                 ans.append(i)
+                concatinations.add(candidate)
             i += 1
         return ans
 
