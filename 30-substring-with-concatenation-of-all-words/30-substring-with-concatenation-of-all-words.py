c302ac559fde2898b6423ee0a6ddfed27ef89394
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-13 17:50:26 +0300

    Time: 1007 ms (36.23%), Space: 14.1 MB (77.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -3,26 +3,29 @@ class Solution:
         len_of_word_in_words = len(words[0])
         concatination_length = len(words) * len_of_word_in_words
         count = Counter(words)
-        i = 0
-        ans = []
+        i, ans = 0, []
         while i + concatination_length <= len(s):
             added = count.copy()
-            c = 1
-            st = ""
-            valid = True
+            c, st, isConcatination = 1, "", True
             for j in range(i, i + concatination_length):
                 st += s[j]
                 if c % len_of_word_in_words == 0:
                     if st in added and added[st] > 0:
                         added[st] -= 1
                         st = ""
-                        
                     else:
-                        valid = False
+                        isConcatination = False
                         break
                 c += 1
-            if valid:
+            if isConcatination:
                 ans.append(i)
             i += 1
         return ans
+
+# time and space complexity
+# n = len(s)
+# m = len(words)
+# l = len(words[i])
+# time: O(lnm)
+# space: O(lnm)
         
