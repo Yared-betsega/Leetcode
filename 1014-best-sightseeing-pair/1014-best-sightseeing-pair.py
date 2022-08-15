5407c5a38766d0036e4afe068945099b2e4cb53a
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-15 16:38:06 +0300

    Time: 721 ms (55.19%), Space: 20.6 MB (27.22%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        i = 0
+        score = 0
+        for j in range(1, len(values)):
+            score = max(score, values[i] + values[j] + (i - j))
+            if values[j] >= values[i]:
+                i = j
+            elif values[i] - values[j] < j - i:
+                i = j
+        return score
+            
