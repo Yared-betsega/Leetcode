aafd7b38510796ae08bc5672b549a7eb7b366f25
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-17 21:43:45 +0300

    Time: 73 ms (27.91%), Space: 14.1 MB (25.86%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,17 +1,21 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
-        @lru_cache
+        memo = {}
         def dp(index):
             if index == len(s):
                 return True
+            if index in memo:
+                return memo[index]
             
             string = ""
             for i in range(index, len(s)):
                 string += s[i]
                 if string in words:
                     if dp(i + 1):
+                        memo[index] = True
                         return True
+            memo[index] = False
             return False
         
         return dp(0)
