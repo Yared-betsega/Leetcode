dfbafc733fcf74eff338c242a903b0aaf42a5db6
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-17 21:13:31 +0300

    Time: 387 ms (5.02%), Space: 22.8 MB (9.17%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -2,19 +2,21 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         isValid = lambda x: 0<=x[0]<len(matrix) and 0<=x[1]<len(matrix)
         n = len(matrix) - 1
-        
-        @cache
+        memo = {}
         def dp(row, col):
             if not isValid((row, col)):
                 return float("inf")
             if row == n:
                 return matrix[row][col]
+            if (row, col) in memo:
+                return memo[(row, col)]
             
             left = dp(row + 1, col - 1)
             down = dp(row + 1, col)
             right = dp(row + 1, col + 1)
             
-            return matrix[row][col] + min(left, down, right)
+            memo[(row, col)] = matrix[row][col] + min(left, down, right)
+            return memo[(row, col)] 
         
         ans = float("inf")
         for i in range(len(matrix)):
