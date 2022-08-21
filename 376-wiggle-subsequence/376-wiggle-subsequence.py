3d581a4f80b5290e4793236100efdce04226b6db
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-21 11:10:10 +0300

    Time: 74 ms (20.04%), Space: 14 MB (34.22%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,19 +1,35 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        dp = [[1, 0]] * len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if j == 0:
-                    if nums[i] != nums[j]:
-                        dp[i] = [dp[j][0] + 1, 1 if nums[i] > nums[j] else -1]
-                elif dp[j][1] == 1:
-                    if nums[i] < nums[j]:
-                        dp[i] = max(dp[i], [dp[j][0] + 1, -1])
-                else:
-                    if nums[i] > nums[j]:
-                        dp[i] = max(dp[i], [dp[j][0] + 1, 1])
-        return max(dp)[0]
+#         dp = [[1, 0]] * len(nums)
+#         for i in range(len(nums)):
+#             for j in range(i):
+#                 if j == 0:
+#                     if nums[i] != nums[j]:
+#                         dp[i] = [dp[j][0] + 1, 1 if nums[i] > nums[j] else -1]
+#                 elif dp[j][1] == 1:
+#                     if nums[i] < nums[j]:
+#                         dp[i] = max(dp[i], [dp[j][0] + 1, -1])
+#                 else:
+#                     if nums[i] > nums[j]:
+#                         dp[i] = max(dp[i], [dp[j][0] + 1, 1])
+#         return max(dp)[0]
     
-# time and space complexity
-# time: O(n**2)
-# space: O(n)
+# # time and space complexity
+# # time: O(n**2)
+# # space: O(n)
+            
+        inc, dec = [1] * len(nums), [1] * len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                inc[i] = dec[i - 1] + 1
+                dec[i] = dec[i - 1]
+            elif nums[i] < nums[i - 1]:
+                dec[i] = inc[i - 1] + 1
+                inc[i] = inc[i - 1]
+            else:
+                inc[i] = inc[i - 1]
+                dec[i] = dec[i - 1]
+        return max(max(inc), max(dec))
+        
+        
+        
