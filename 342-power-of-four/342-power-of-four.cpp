f7a18edba8ef20a59a8dc7308b3c064c33a26c70
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-22 10:40:50 +0300

    Time: 3 ms (58.67%), Space: 5.8 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool isPowerOfFour(double n) {
+    bool isPowerOfFour(int n) {
         if (n == 1){
             return true;
         }
-        else if (n < 1){
+        else if (n % 4 != 0 || n < 1){
             return false;
         }
         else{
-            return isPowerOfFour(n / 4.0);
+            return isPowerOfFour(n / 4);
         }
     }
 };
