f35605262667ae96ff5f6f1376927f3dd309da0e
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-23 23:57:44 +0300

    Time: 90 ms (63.05%), Space: 15.2 MB (24.31%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        will_buy = prices[0]
-        ans = []
-        dp = 0
-        for i in range(1, len(prices)):
-            if prices[i] < prices[i - 1]:
-                heappush(ans, -dp)
-                dp = 0
-                will_buy = prices[i]
-            else:
-                dp = max(dp, prices[i] - will_buy)
-        heappush(ans, -dp)
-        return -sum(ans)
+#         will_buy = prices[0]
+#         ans = []
+#         dp = 0
+#         for i in range(1, len(prices)):
+#             if prices[i] < prices[i - 1]:
+#                 heappush(ans, -dp)
+#                 dp = 0
+#                 will_buy = prices[i]
+#             else:
+#                 dp = max(dp, prices[i] - will_buy)
+#         heappush(ans, -dp)
+#         return -sum(ans)
 
-# time and space complexity
-# time: O(nlog(n))
-# space: O(n)
+# # time and space complexity
+# # time: O(nlog(n))
+# # space: O(n)
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
