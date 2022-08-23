f3b57b2f06fd2852fe6178495412f3396c31bbe8
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-23 23:58:44 +0300

    Time: 103 ms (46.12%), Space: 15.1 MB (69.49%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -16,8 +16,13 @@ class Solution:
 # # time and space complexity
 # # time: O(nlog(n))
 # # space: O(n)
+
         ans = 0
         for i in range(1, len(prices)):
             if prices[i] > prices[i - 1]:
                 ans += prices[i] - prices[i - 1]
         return ans
+
+# time and space complexity
+# time: O(n)
+# space: O(1)
