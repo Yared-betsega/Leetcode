3b0836ff0fd147cad1fc29eb60160fb9a8fd8df8
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-23 23:48:51 +0300

    Time: 150 ms (6.24%), Space: 15.2 MB (69.49%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        will_buy = prices[0]
+        ans = []
+        dp = 0
+        for i in range(1, len(prices)):
+            if prices[i] < prices[i - 1]:
+                heappush(ans, -dp)
+                dp = 0
+                will_buy = prices[i]
+            else:
+                dp = max(dp, prices[i] - will_buy)
+        heappush(ans, -dp)
+        return -sum(ans)
