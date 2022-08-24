be187bd7c408e0934bca812a24c05f694fa8b739
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-24 14:42:55 +0300

    Time: 7035 ms (9.67%), Space: 19.2 MB (5.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,13 +1,29 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[j] + 1, dp[i])
+#         dp = [1] * len(nums)
+#         for i in range(1, len(nums)):
+#             for j in range(i):
+#                 if nums[i] > nums[j]:
+#                     dp[i] = max(dp[j] + 1, dp[i])
         
-        return max(dp)
+#         return max(dp)
 
-# time and space complexity
-# time: O(n**2)
-# space: O(n)
+# # time and space complexity
+# # time: O(n**2)
+# # space: O(n)
+        @cache
+        def dfs(index):
+            c = 0
+            ans = 0
+            for i in range(index + 1, len(nums)):
+                if nums[i] > nums[index]:
+                    ans = max(ans, dfs(i) + 1)
+                    c += 1
+            if c == 0:
+                return 1
+            else:
+                return ans
+        ans = 0
+        for i in range(len(nums)):
+            ans = max(ans, dfs(i))
+        return ans
