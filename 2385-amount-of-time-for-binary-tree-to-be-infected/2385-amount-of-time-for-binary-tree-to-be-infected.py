8bcc14765b276c38ab2e68ebc2ad0d57fa893d61
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-26 01:30:39 +0300

    Time: 781 ms (100.00%), Space: 134.7 MB (75.00%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -7,25 +7,28 @@
 class Solution:
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
         parents = []
-        def dfs(node):
+        # To collect the parents of the start node
+        def collectParent(node):
             parents.append(node)
             if node.val == start:
                 return True
             found = False
             if node.left:
-                found = found or dfs(node.left)
+                found = collectParent(node.left)
             if node.right:
-                found = found or dfs(node.right)
+                found = found or collectParent(node.right)
             if found:
                 return True
             parents.pop()
             return False
+        
+        # Count the distance of he farthest node to the current parrent
         def count(node):
             if not node or node.val in visited:
                 return 0
             return 1 + max(count(node.left), count(node.right))
         
-        dfs(root)
+        collectParent(root)
         visited = set()
         dis = maxLen = 0
         for parent in parents[::-1]:
