3e862c292797ec7cc7e24b7e69cf811abebbcbc0
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-30 22:45:40 +0300

    Time: 101 ms (13.33%), Space: 16.3 MB (84.64%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -14,29 +14,9 @@ class Solution:
                 return 1
             left = dfs(node.left)
             right = dfs(node.right)
-            # print(left + right, node.val)
             _max[0] = max(_max[0], left + right)
             return max(left, right) + 1
         
         dfs(root)
         return _max[0]
             
-        
-#         """
-#         1: left = 2, right = 1 = 3
-#         2: left = 1, right = 1 = 2
-#         4: left = 0, right = 0 = 0
-#         """
-#         1-2, 2-4, 
-#         time: O(n)
-#         recursion
-#         psedocode:
-#         def rec():
-#             if node is leaf:
-#                 return 1
-#             left <= rec(left)
-#             rigth <= rec(right)
-#             glomax = max(glomax, left + right)
-#             return max(left, right)
-            
-        
