2b0ffa7ae706a886971ba956b182151b925bba87
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-08-31 11:36:15 +0300

    Time: 154 ms (53.90%), Space: 18.1 MB (5.18%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        if not prerequisites:
+            return True
+        
+        graph = defaultdict(list)
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+        
+        def dfs(node, path):
+            path.add(node)
+            self.visited.add(node)
+
+            for neighbor in graph[node]:
+                if neighbor in path:
+                    self.hasCycle = True
+                if neighbor not in self.visited:
+                    dfs(neighbor, path)
+                    
+            path.remove(node)
+            self.answer += 1
+
+        self.answer = 0
+        self.hasCycle = False
+        self.visited = set()
+        
+        for course in range(numCourses):
+            if course not in self.visited:
+                dfs(course, set())
+        
+        return not self.hasCycle and self.answer == numCourses
+                
+# time and space complexity 
+# time complexity = O(V+E)
+# space complexity = O(V)
+# v = len(vertices), e = len(edges)
+                
+                
