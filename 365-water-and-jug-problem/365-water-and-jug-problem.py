c87e85dd9184bc66a3d6f1817edfb8d09e31cec6
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-07 00:07:01 +0300

    Time: 45 ms (76.24%), Space: 13.9 MB (83.80%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.py
+++ 365-water-and-jug-problem/365-water-and-jug-problem.py
@@ -9,4 +9,9 @@ class Solution:
         
         gcd = greatestCommonDevisor(jug1Capacity, jug2Capacity)
         return True if targetCapacity % gcd == 0 else False
-        
+
+# time and space complexity
+# n = max(jug1Capacity, jug2Capacity)
+# time: O(logn)
+# space: O(1)
+    
