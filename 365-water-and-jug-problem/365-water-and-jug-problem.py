15f860473f7a6812b4c932f636a18a8c527db6ba
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-07 00:03:07 +0300

    Time: 26 ms (98.56%), Space: 13.9 MB (83.80%) - LeetHub

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        if jug1Capacity + jug2Capacity < targetCapacity:
+            return False
+        def greatestCommonDevisor(x, y):
+           while(y):
+               x, y = y, x % y
+           return x
+        
+        gcd = greatestCommonDevisor(jug1Capacity, jug2Capacity)
+        return True if targetCapacity % gcd == 0 else False
+        
