2482bec15b28447d5fc5a0dfde187c8c88575054
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-10 00:05:18 +0300

    Time: 56 ms (39.70%), Space: 13.9 MB (85.61%) - LeetHub

---
+++ 1138-alphabet-board-path/1138-alphabet-board-path.py
@@ -0,0 +1,32 @@
+class Solution:
+    def alphabetBoardPath(self, target: str) -> str:
+        ans = []
+        n = len(target)
+        curPos = [0, 0]
+        for i in range(n):
+            ordDif = ord(target[i]) - ord("a")
+            verPos = ordDif // 5
+            horPos = ordDif - ((verPos) * 5)
+            
+            verMove = []
+            if curPos[0] > verPos:
+                verMove = ["U"] * (curPos[0] - verPos)
+            elif curPos[0] < verPos:
+                verMove = ["D"] * (verPos - curPos[0])
+                
+            horMove = [] 
+            if curPos[1] < horPos:
+                horMove = ["R"] * (horPos - curPos[1])
+            elif curPos[1] > horPos:
+                horMove = ["L"] * (curPos[1] - horPos)
+            
+            if verPos < 5:
+                ans.extend(verMove + horMove)
+            else:
+                ans.extend(horMove + verMove)
+            
+            ans.append("!")
+            curPos = [verPos, horPos]
+            
+        return "".join(ans)
+                
