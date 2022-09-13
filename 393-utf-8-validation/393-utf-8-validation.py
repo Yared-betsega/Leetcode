a19691caeefb11869566c1ad42451b31627e3a85
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-13 20:25:19 +0300

    Time: 271 ms (14.18%), Space: 14.2 MB (70.76%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        i = 0
+        jumpState = 0
+        while i < len(data):
+            rep = bin(data[i])[2:]
+            binary = "0" * (8 - len(rep)) + rep
+            if jumpState:
+                if binary[:2] != "10":
+                    return False
+                jumpState -= 1
+            else:
+                zero = binary.find("0")
+                if zero == -1 or zero == 1 or zero > 4:
+                    return False
+                elif zero > 0: 
+                    jumpState = zero - 1
+            i += 1
+        return True if jumpState == 0 else False
