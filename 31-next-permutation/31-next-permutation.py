80973185ef3bc42668974eb08be5c0f9e44de74a
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-19 22:55:32 +0300

    Time: 81 ms (29.54%), Space: 13.9 MB (74.30%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        found = False
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                _max = i + 1
+                for j in range(i + 1, len(nums)):
+                    if nums[j] > nums[i] and nums[j] < nums[_max]:
+                        _max = j
+                elm = nums.pop(_max)
+                nums.insert(i, elm)
+                temp = nums[i + 1: ]
+                temp.sort()
+                n = 0
+                for j in range(i + 1, len(nums)):
+                    nums[j] = temp[n]
+                    n += 1
+                found = True
+                break
+                        
+        if not found:
+            nums.sort()
+            
