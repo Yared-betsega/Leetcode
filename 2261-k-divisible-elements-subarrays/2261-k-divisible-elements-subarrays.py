d8b0906383014edaae15a3275b369af37dcdac58
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-21 17:13:05 +0300

    Time: 1284 ms (44.93%), Space: 30 MB (5.53%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.py
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.py
@@ -1,8 +1,8 @@
 class Solution:
     def countDistinct(self, nums: List[int], k: int, p: int) -> int:
         l = r = 0
-        divs = ans = 0
-        ans = set()
+        divs = 0
+        pairs = []
         for i in range(len(nums)):
             divs = 0
             for j in range(i, len(nums)):
@@ -10,5 +10,12 @@ class Solution:
                     divs += 1
                 if divs > k:
                     break
-                ans.add(tuple(nums[i: j + 1]))
-        return len(ans)
+                pairs.append((i, j + 1))
+        subs = set()
+        ans = 0
+        for i, j in pairs:
+            subarray = tuple(nums[i: j])
+            if subarray not in subs:
+                ans += 1
+                subs.add(subarray)
+        return ans
