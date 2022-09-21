03060a9f2e29672acb584e68be9c71327724a1ef
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-21 21:30:41 +0300

    Time: 115 ms (13.11%), Space: 14.7 MB (11.89%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pos = {0: -1}
+        node = head
+        _sum = index = 0
+        toBeRemoved = []
+        while node:
+            _sum += node.val
+            if _sum in pos:
+                toBeRemoved.append((pos[_sum] + 1, index))
+            pos[_sum] = index
+            node = node.next
+            index += 1
+        if not toBeRemoved:
+            return head
+        toBeRemoved.sort(key = lambda x: (x[0], -x[1]))
+        merged = [toBeRemoved[0]]
+        remove = set([i for i in range(toBeRemoved[0][0], toBeRemoved[0][1] + 1)])
+        for i in range(1, len(toBeRemoved)):
+            if toBeRemoved[i][0] > merged[-1][1]:
+                merged.append(toBeRemoved[i])
+                for i in range(toBeRemoved[i][0], toBeRemoved[i][1] + 1):
+                    remove.add(i)
+        prev = start = ListNode()
+        i = 0
+        node = head
+        while node:
+            if i not in remove:
+                prev.next = node
+                prev = prev.next
+            node = node.next
+            i += 1
+        prev.next = None
+        return start.next
