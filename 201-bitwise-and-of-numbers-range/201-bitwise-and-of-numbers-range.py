72f1eeb22dd66d0511403d42d54708529b7d6450
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-22 20:32:10 +0300

    Time: 241 ms (5.05%), Space: 13.8 MB (98.17%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
@@ -2,7 +2,16 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         if left == right:
             return left
-        maxBinLength = len(bin(right)[2:])
+        
+        # To get the shortest lenght of the binary representation of the largest element
+        # Takes O(32) ~ O(1) time complexity
+        for i in range(32):
+            if right & (1 << i):
+                maxBinLength = 32 - i
+                break
+        
+        # Add check if each bit index in the above maxBinLength are on or off 
+        # if it is on make the corresponding bit index of the answer on
         ans = cur = 0
         for i in range(maxBinLength - 1, -1, -1):
             if right & (1 << i):
@@ -10,4 +19,8 @@ class Solution:
                 if left >= cur:
                     ans += (1 << i)
         return ans
+
+# time and space complexity
+# time: O(32)
+# space: O(1)
                 
