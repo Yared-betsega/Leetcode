acf5f62c827c452e640636f86741c59c8c8fd0a3
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-24 00:23:53 +0300

    Time: 8237 ms (5.04%), Space: 96.7 MB (5.03%) - LeetHub

---
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        mins, lefts, _leftSum = [], {}, 0
+        for i in range(len(nums)):
+            heappush(mins, -nums[i])
+            _leftSum += nums[i]
+            if len(mins) > len(nums) / 3:
+                _leftSum -= -heappop(mins)
+            
+            lefts[i] = _leftSum
+        
+        maxes, rights, _rightSum = [], {}, 0
+        for i in range(len(nums) - 1, -1, -1):
+            rights[i] = _rightSum
+            heappush(maxes, nums[i])
+            _rightSum += nums[i]
+            if len(maxes) > len(nums) / 3:
+                _rightSum -= heappop(maxes)
+            
+        # print(lefts, rights) 
+        ans = float("inf")
+        for i in range(len(nums)):
+            if i + 1 >= len(nums) / 3 and len(nums) - i > len(nums) / 3:
+                # print(i)
+                ans = min(ans, lefts[i] - rights[i])
+        return ans
+        
