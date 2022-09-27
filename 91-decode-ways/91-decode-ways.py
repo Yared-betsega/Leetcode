b7a7bf2d11011f8f689437df939b6f92963d5fb3
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-27 18:44:13 +0300

    Time: 41 ms (79.77%), Space: 13.8 MB (98.59%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,5 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # Bottom up solution
+        dp = [0] * (len(s) + 1)
+        dp[len(s) - 1] = 1 if s[-1] != "0" else 0
+        dp[-1] = 1
+        for i in range(len(s) - 2, -1, -1):
+            if int(s[i]) != 0:
+                dp[i] += dp[i + 1]
+                if int(s[i: i + 2]) <= 26:
+                    dp[i] += dp[i + 2]
+        
+        return dp[0]
+            
+        '''
         memo = {}
         def dp(idx):
             if idx == len(s) - 1:
@@ -19,3 +32,5 @@ class Solution:
 # time and space complexity
 # time: O(n)
 # space: O(n)
+
+        '''
