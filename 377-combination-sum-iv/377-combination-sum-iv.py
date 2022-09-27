0ac26b245e0bbf5ab6ef556f5a419b17e7c2e799
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-28 01:20:01 +0300

    Time: 98 ms (7.65%), Space: 13.9 MB (81.71%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -4,12 +4,12 @@ class Solution:
         dp = [0] * (target + 1)
         dp[0] = 1
         for i in range(target):
-            if dp[i] != float("inf"):
-                for num in nums:
-                    if i + num <= target:
-                        dp[i + num] += dp[i]
+            for num in nums:
+                if i + num <= target:
+                    dp[i + num] += dp[i]
         return dp[target]
-
+    
+        # Top down approach
         memo = {}
         def helper(target):
             if target < 0:
