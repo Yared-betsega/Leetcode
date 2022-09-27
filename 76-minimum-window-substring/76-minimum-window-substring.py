1ec58f0780e51831d5503bf9fedb9810e478ceb8
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-28 01:07:45 +0300

    Time: 781 ms (9.07%), Space: 14.7 MB (83.77%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def check():
+            for i in tcount:
+                if tcount[i] > scount[i]:
+                    return False
+            return True
+        
+        tcount = Counter(t)
+        uni = set(t)
+        scount = defaultdict(int)
+        l = r = 0
+        ans = [float("inf"), (-1, -1)]
+        while r < len(s):
+            while l < r:
+                if s[l] not in uni:
+                    l += 1
+                elif s[l] in uni and tcount[s[l]] < scount[s[l]]:
+                    scount[s[l]] -= 1
+                    l += 1
+                else:
+                    break
+                
+            scount[s[r]] += 1
+            # print(scount, ans)
+            if check():
+                ans = min([r - l + 1, (l, r)], ans)
+                while l < r and check():
+                    scount[s[l]] -= 1
+                    l += 1
+                
+            r += 1
+            # print(l, r)
+        
+        return s[ans[1][0]: ans[1][1] + 1]
