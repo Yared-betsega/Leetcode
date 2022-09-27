83e2ae2255bb9f57faf6638216ac485d7017bc6f
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-09-27 15:36:13 +0300

    Time: 1087 ms (19.64%), Space: 20.9 MB (28.97%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        forceFromLeft = [0] * len(dominoes)
+        last = -float("inf")
+        for i in range(len(dominoes)):
+            if dominoes[i] == ".":
+                forceFromLeft[i] = i - last
+            elif dominoes[i] == "R":
+                last = i
+            else:
+                last = -float("inf")
+            
+        forceFromRight = [0] * len(dominoes)
+        last = float("inf")
+        for i in range(len(dominoes) - 1, -1, -1):
+            if dominoes[i] == ".":
+                forceFromRight[i] = last - i
+            elif dominoes[i] == "L":
+                last = i
+            else:
+                last = float("inf")
+                
+        dominoes = list(dominoes)    
+        for i in range(len(dominoes)):
+            if dominoes[i] == ".":
+                if forceFromRight[i] < forceFromLeft[i]:
+                    dominoes[i] = "L"
+                elif forceFromRight[i] > forceFromLeft[i]:
+                    dominoes[i] = "R"
+        
+        return "".join(dominoes)
