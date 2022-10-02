393fae35f3f3299ddf04395bb06220c2344f8d57
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-02 22:22:07 +0300

    Time: 188 ms (13.35%), Space: 17.6 MB (5.34%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,25 +1,20 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        isValid = lambda x: 0 <= x[0] < len(dungeon) and 0 <= x[1] < len(dungeon[0])
-        @cache
-        def dp(r, c, life):
-            if isValid((r, c)):
-                life += dungeon[r][c]
-                if life <= 0:
-                    return False
-                if r == len(dungeon) - 1 and c == len(dungeon[0]) - 1:
-                    return True
-                return dp(r + 1, c, life) or dp(r, c + 1, life)
-            return False
-        
         m, n = len(dungeon), len(dungeon[0])
-        l, r = 1, m * n * (1001)
-        while l < r:
-            m = (l + r) // 2
-            if dp(0, 0, m):
-                r = m
-            else:
-                l = m + 1
-                
-        return r
-            
+        isValid = lambda x: 0 <= x[0] < m and 0 <= x[1] < n
+        @cache
+        def dp(r, c):
+            if isValid((r, c)):
+                if r == m - 1 and c == n - 1:
+                    if dungeon[r][c] <= 0:
+                        return dungeon[r][c]
+                    return 0
+                after = dungeon[r][c] +  max(dp(r + 1, c), dp(r, c + 1))
+                if after <= 0:
+                    return after
+                return 0
+            return -float("inf")
+        # print(dp(0, 0))
+        ans = -dp(0, 0) + 1
+        return ans if ans != float("inf") else 1
+        
