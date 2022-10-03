15da55f1f5bfc09c26b353e872eed0bfeb08f526
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-03 12:08:39 +0300

    Time: 3253 ms (9.81%), Space: 28.3 MB (91.39%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
@@ -9,8 +9,12 @@ class Solution:
         for i in range(len(prefix)):
             while queue and prefix[i] <= prefix[queue[-1]]:
                 queue.pop()
-            queue.append(i)
             while queue and prefix[i] - prefix[queue[0]] >= k :
                 ans = min(ans, i - queue.popleft())
-            # print(queue)
+            queue.append(i)
+
         return ans if ans != float("inf") else -1
+
+# time and space complexity
+# time: O(n)
+# space: O(n)
