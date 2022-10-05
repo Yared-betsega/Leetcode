582b63350bc2ac70d893cfa6cbbc47fdcdd83e1c
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-05 20:58:51 +0300

    Time: 94 ms (36.24%), Space: 15.7 MB (5.29%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -3,18 +3,31 @@ class Solution:
         days = set(days)
         dur = [1, 7, 30]
         @cache
-        def dp(day, rem):
+        def dp(day):
             if day > 365:
                 return 0
             if day not in days:
-                return dp(day + 1, max(0, rem - 1))
-            if rem > 0:
-                return dp(day + 1, rem - 1)
-            
+                return dp(day + 1)
             ans = float("inf")
             for i in range(3):
-                ans = min(ans, costs[i] + dp(day + 1, dur[i] - 1))
+                ans = min(ans, costs[i] + dp(day + dur[i]))
             return ans
         
-        return dp(1, 0)
+        return dp(1)
+#         days = set(days)
+#         dur = [1, 7, 30]
+#         dp = [0] * 365
+#         i = 0
+#         while i <= 365:
+#             if i in days:
+#                 start = dp[i]
+#                 for j in range(3):
+#                     for k in range(i, min(365, i + dur[j])):
+#                         if dp[k] == float("inf"):
+#                             dp[k] = costs[j]
+#                         dp[k] = min(dp[k], start + costs[j])
+#                 i = k + 1
+#             else:
+#                 i += 1
+
                 
