6ed5733f1ed283ac787061a272200e8eafb33850
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-05 22:17:52 +0300

    Time: 53 ms (86.02%), Space: 13.8 MB (91.13%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,33 +1,26 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        days = set(days)
-        dur = [1, 7, 30]
-        @cache
-        def dp(day):
-            if day > 365:
-                return 0
-            if day not in days:
-                return dp(day + 1)
-            ans = float("inf")
-            for i in range(3):
-                ans = min(ans, costs[i] + dp(day + dur[i]))
-            return ans
-        
-        return dp(1)
 #         days = set(days)
 #         dur = [1, 7, 30]
-#         dp = [0] * 365
-#         i = 0
-#         while i <= 365:
-#             if i in days:
-#                 start = dp[i]
-#                 for j in range(3):
-#                     for k in range(i, min(365, i + dur[j])):
-#                         if dp[k] == float("inf"):
-#                             dp[k] = costs[j]
-#                         dp[k] = min(dp[k], start + costs[j])
-#                 i = k + 1
-#             else:
-#                 i += 1
-
-                
+#         @cache
+#         def dp(day):
+#             if day > 365:
+#                 return 0
+#             if day not in days:
+#                 return dp(day + 1)
+#             ans = float("inf")
+#             for i in range(3):
+#                 ans = min(ans, costs[i] + dp(day + dur[i]))
+#             return ans
+#         return dp(1)
+        n = days[-1]
+        days = set(days)
+        dp = [0] * (n + 1)
+        for i in range(1, len(dp)):
+            if i in days:
+                for j in range(3):
+                    dp[i] = min(dp[i - 1] + costs[0], dp[max(i - 7, 0)] + costs[1], dp[max(i - 30, 0)] + costs[2])
+            else:
+                dp[i] = dp[i - 1]
+        return dp[-1]
+            
