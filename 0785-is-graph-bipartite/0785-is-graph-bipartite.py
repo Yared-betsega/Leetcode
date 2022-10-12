f594a342259889317b9d06ccb4b2ce57230a70b0
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-12 15:28:47 +0300

    Time: 525 ms (9.68%), Space: 15.1 MB (5.35%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color = defaultdict(lambda : -1)
+        def bfs(root):
+            queue = deque([(root, 0)])
+            color[root] = 0
+            while queue:
+                cur, clr = queue.popleft()
+                color[cur] = clr
+                for nxt in graph[cur]:
+                    if color[cur] == color[nxt]:
+                        return False
+                    if color[nxt] == -1:
+                        queue.append((nxt, not color[cur]))
+            return True
+        
+        for i in range(len(graph)):
+            if color[i] == -1:
+                if not bfs(i):
+                    return False
+        return True
+                        
