3056cf0c5e70787d09f1e176a0a4ba4bbfa3cea6
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-14 14:50:05 +0300

    Time: 4361 ms (83.40%), Space: 14 MB (12.47%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -13,7 +13,7 @@ class Solution:
         DIR = [(0,1), (0,-1), (1,0), (-1,0)]
         isValid = lambda x: 0 <= x[0] < len(board) and 0 <= x[1] < len(board[0])
         
-        def dfs(r, c, i, path):
+        def dfs(r, c, i):
             
             if i == len(word) - 1 and board[r][c] == word[i]:
                 return True
@@ -24,13 +24,13 @@ class Solution:
             for dx, dy in DIR:
                 nx, ny = r + dx, c + dy
                 if isValid((nx, ny)):
-                    if dfs(nx, ny, i + 1, path):
+                    if dfs(nx, ny, i + 1):
                         return True
             board[r][c] = temp
             
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if dfs(i, j, 0, set()):
+                    if dfs(i, j, 0):
                         return True
         return False
