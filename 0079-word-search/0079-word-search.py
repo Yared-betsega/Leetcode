e9bb8f64c7a81471e751da612fe5ab8cd9d082b5
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-14 14:31:17 +0300

    Time: 7569 ms (40.73%), Space: 13.9 MB (50.91%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # wordCt = Counter(word)
+        # boardCt = defaultdict(int)
+        # for i in range(len(board)):
+        #     for j in range(len(board[0])):
+        #         boardCt[i][j] += 1
+                
+                
+                
+                
+        DIR = [(0,1), (0,-1), (1,0), (-1,0)]
+        isValid = lambda x: 0 <= x[0] < len(board) and 0 <= x[1] < len(board[0])
+        
+        def dfs(r, c, i, path):
+            
+            if i == len(word) - 1 and board[r][c] == word[i]:
+                return True
+            if board[r][c] != word[i]:
+                return False
+            
+            path.add((r, c))
+            for dx, dy in DIR:
+                nx, ny = r + dx, c + dy
+                if isValid((nx, ny)) and (nx, ny) not in path:
+                    if dfs(nx, ny, i + 1, path):
+                        return True
+            path.remove((r, c))
+            
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0, set()):
+                        return True
+        return False
