4c65d7875ec2a17685ff45d2a964bdcfa5ec5a23
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-17 21:55:19 +0300

    Time: 4524 ms (24.63%), Space: 66 MB (9.98%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        rides.sort()
+        startPoints = list(map(lambda x: x[0], rides))
+        
+        @cache
+        def pickPassenger(i):
+            if i >= len(rides):
+                return 0
+            pick = rides[i][1] - rides[i][0] + rides[i][2] + pickPassenger(bisect_left(startPoints, rides[i][1]))
+            dontPick = pickPassenger(i + 1)
+            return max(pick, dontPick)
+        
+        return pickPassenger(0)
