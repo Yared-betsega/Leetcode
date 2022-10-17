ae0a00ec076e1e14538696478f1674b312e7336d
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-17 22:12:13 +0300

    Time: 4055 ms (40.11%), Space: 33.6 MB (65.99%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -3,14 +3,24 @@ class Solution:
         rides.sort()
         startPoints = list(map(lambda x: x[0], rides))
         
-        @cache
-        def pickPassenger(i):
-            if i >= len(rides):
-                return 0
-            nextIndex = bisect_left(startPoints, rides[i][1])
-            pick = (rides[i][1] - rides[i][0] + rides[i][2]) + pickPassenger(nextIndex)
-            dontPick = pickPassenger(i + 1)
-            return max(pick, dontPick)
+#         @cache
+#         def pickPassenger(i):
+#             if i >= len(rides):
+#                 return 0
+#             nextIndex = bisect_left(startPoints, rides[i][1])
+#             pick = (rides[i][1] - rides[i][0] + rides[i][2]) + pickPassenger(nextIndex)
+#             dontPick = pickPassenger(i + 1)
+#             return max(pick, dontPick)
         
-        return pickPassenger(0)
+#         return pickPassenger(0)
+    
+        # Bottom Up solution
+        pickPassenger = [0] * (len(rides) + 1)
+        for i in range(len(rides) - 1, -1, -1):
+            nextIndex = bisect_left(startPoints, rides[i][1])
+            pick = rides[i][1] - rides[i][0] + rides[i][2] + pickPassenger[nextIndex]
+            dontPick = pickPassenger[i + 1]
+            pickPassenger[i] = max(pick, dontPick)
+            
+        return pickPassenger[0]
                     
