7907f7302da862c52e78035e3e4d09947ce52169
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-17 10:38:55 +0300

    Time: 855 ms (92.00%), Space: 15 MB (52.82%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -2,15 +2,25 @@ class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
         if d > len(jobDifficulty):
             return -1
-        @cache
-        def dp(i, d, _mx):
-            if i >= len(jobDifficulty):
-                if not d: return _mx
-                else: return float("inf")     
-            if d < 0: return float("inf")
+#         @cache
+#         def dp(i, d, _mx):
+#             if i >= len(jobDifficulty):
+#                 if not d: return _mx
+#                 else: return float("inf")     
+#             if d < 0: return float("inf")
             
             
-            _mx = max(_mx, jobDifficulty[i])
-            return min(dp(i + 1, d, _mx), _mx + dp(i + 1, d - 1, 0))
+#             _mx = max(_mx, jobDifficulty[i])
+#             return min(dp(i + 1, d, _mx), _mx + dp(i + 1, d - 1, 0))
     
-        return dp(0, d, 0)
+#         return dp(0, d, 0)
+        @cache
+        def dp(i, day):
+            if day == d: return max(jobDifficulty[i:])
+            ans, curMax = float("inf"), 0
+            for j in range(i, len(jobDifficulty) - d + day):
+                curMax = max(curMax, jobDifficulty[j])
+                ans = min(ans, curMax + dp(j + 1, day + 1))
+            return ans
+        
+        return dp(0, 1)
