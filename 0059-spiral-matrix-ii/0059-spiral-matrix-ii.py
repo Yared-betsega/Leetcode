d4c9e938e02974c23bf180adb5eeb7b5db385d8d
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-18 15:57:09 +0300

    Time: 28 ms (98.28%), Space: 14 MB (9.41%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        leftIndex = 0
+        rightIndex = n - 1
+        bottomIndex = n - 1
+        topIndex = 0
+        currentNumber = 1
+        matrix = [[0 for i in range(n)] for i in range(n)]
+        
+        while topIndex <= bottomIndex and leftIndex <= rightIndex:
+            for i in range(leftIndex, rightIndex + 1):
+                matrix[topIndex][i] = currentNumber
+                currentNumber += 1
+            topIndex += 1
+            
+            for i in range(topIndex, bottomIndex + 1):
+                matrix[i][rightIndex] = currentNumber
+                currentNumber += 1
+            rightIndex -= 1
+            
+            if topIndex > bottomIndex or leftIndex > rightIndex:
+                break
+            
+            for i in range(rightIndex, leftIndex - 1, -1):
+                matrix[bottomIndex][i] = currentNumber
+                currentNumber += 1
+            bottomIndex -= 1
+            
+            for i in range(bottomIndex, topIndex - 1, -1):
+                matrix[i][leftIndex] = currentNumber
+                currentNumber += 1
+            leftIndex += 1
+            
+        return matrix
