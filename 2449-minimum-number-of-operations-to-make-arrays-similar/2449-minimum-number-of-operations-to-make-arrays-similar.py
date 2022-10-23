085ffb3649d60d918f2e8a540f65e3bc4539b725
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-23 15:16:27 +0300

    Time: 1155 ms (91.67%), Space: 34.3 MB (8.33%) - LeetHub

---
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makeSimilar(self, nums: List[int], target: List[int]) -> int:
+        evenNums = []
+        evenTargets = []
+        oddNums = []
+        oddTargets = []
+        for num, tar in zip(nums, target):
+            evenNums.append(num) if not num & 1 else oddNums.append(num)
+            evenTargets.append(tar) if not tar & 1 else oddTargets.append(tar)
+            
+        heapify(evenNums)
+        heapify(evenTargets)
+        answer = 0
+        while evenNums:
+            first, second = heappop(evenNums), heappop(evenTargets)
+            if first > second:
+                answer += first - second
+        
+        heapify(oddNums)
+        heapify(oddTargets)
+        while oddNums:
+            first, second = heappop(oddNums), heappop(oddTargets)
+            if first > second:
+                answer += first - second
+        
+        return answer // 2
