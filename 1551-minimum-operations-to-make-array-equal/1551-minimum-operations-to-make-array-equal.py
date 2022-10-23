29c4767ad9b113f5cb56494436864c9cfaf9d0ec
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-10-23 15:43:42 +0300

    Time: 55 ms (75.81%), Space: 13.8 MB (65.65%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,16 +1,12 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        target = None
         if n % 2 == 0:
-            target = ((2 * (n // 2)) + 1 + ((2 * (n // 2 - 1))) + 1) // 2
+            return (n // 2) ** 2            
         else:
-            target = (2 * (n // 2)) + 1
+            return ((n // 2) ** 2) + (n // 2)
         
-        answer = 0
-        for i in range(n // 2, n):
-            answer += (2 * i + 1) - target
-        return answer
+        
     
 # time and space complexity
-# time: O(n)
+# time: O(1)
 # space: O(1)
