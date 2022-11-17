3d9269b0299ed6bb93e774df6be35fb0280ffcf8
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-11-17 17:18:25 +0300

    Time: 8570 ms (7.61%), Space: 148.2 MB (5.43%) - LeetHub

---
+++ 1655-distribute-repeating-integers/1655-distribute-repeating-integers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canDistribute(self, nums: List[int], quantity: List[int]) -> bool:
+        count = Counter(nums)
+        count = list(count.values())
+        satisfied = (1 << len(quantity)) - 1
+        
+        @cache
+        def dp(i, recieved, used):
+            if recieved == satisfied:
+                return True
+            if i == len(count):
+                return False
+            dontUse = dp(i + 1, recieved, 0)
+            if dontUse:
+                return True
+            for q in range(len(quantity)):
+                if not recieved & (1 << q):
+                    if quantity[q] <= count[i] - used:
+                        if dp(i, recieved | (1 << q), used + quantity[q]):
+                            return True
+            return False
+        
+        return dp(0, 0, 0)
+                
+        
