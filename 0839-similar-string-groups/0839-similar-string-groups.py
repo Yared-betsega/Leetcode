9556c78e0ef76321378e29f0d97e982d387fd4ed
Yared <83911480+Yared-betsega@users.noreply.github.com>
2022-12-02 15:09:26 +0300

    Time: 5742 ms (17.40%), Space: 14.2 MB (66.90%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -0,0 +1,49 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.size = defaultdict(int)
+    
+    def find(self, x):
+        if x not in self.parent:
+            return x
+        
+        if x == self.parent[x]:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def merge(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px != py:
+            if self.size[px] < self.size[py]:
+                px, py = py, px
+            self.parent[py] = px
+            self.size[px] += self.size[py]
+            
+        
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        strs = list(set(strs))
+        unionfind = UnionFind()
+        for i in range(len(strs)):
+            for j in range(i + 1, len(strs)):
+                if self.check(strs[i], strs[j]):
+                    unionfind.merge(strs[i], strs[j])
+        
+        groups = 0
+        for st in strs:
+            if st == unionfind.find(st):
+                groups += 1
+                
+        return groups
+    
+    def check(self, s1, s2):
+        diff = 0
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                diff += 1
+        
+        return diff == 2
+    
+                
