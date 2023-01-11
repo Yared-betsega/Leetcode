7c9b72f112fead1b119b831ce4c15f16dd31dca6
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-01-11 10:08:43 +0300

    Time: 1431 ms (74.61%), Space: 157 MB (44.20%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,21 +1,29 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        color = [0] * len(edges)
-        longestCycle = [-1]
+        '''
+        Color Definition:
+            -1: Black (no cycle afterwards)
+            0: white (First visit)
+            otherwise: Grey (there is a cycle), The value represents the number of nodes previously visited.
+        '''
+        color = [0] * len(edges) # Initially all elements are not visited
+        
+        longestCycle = [-1] # intitialize longest cycle to be smallest value
         
         def topSort(node, preCount):
-            if color[node] == -1:
+            if color[node] == -1: # No cycle afterwards so no value proceeding
                 return False
-            if color[node] != 0:
+            if color[node] != 0: # Has cycle, update the value of longest cycle to be the max of itself or the length of the current cycle
                 longestCycle[0] = max(longestCycle[0], preCount - color[node])
                 return True
-            color[node] = preCount
+            
+            color[node] = preCount # Mark the current node to be visited
             if edges[node] != -1:
                 hasCycle = topSort(edges[node], preCount + 1)
                 if hasCycle:
                     return True
             
-            color[node] = -1
+            color[node] = -1  # If no cycle, mark the node to black and return
             return False
         
         for i in range(len(edges)):
