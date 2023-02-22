bdf2f39e5a235cbf45080677cc7a89e16210b498
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-02-22 15:39:34 +0300

    Time: 38 ms (83.68%), Space: 14.1 MB (83.13%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        match, taken = {}, set()
+        for i in range(len(s)):
+            if s[i] in match:
+                if match[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] not in taken:
+                    match[s[i]] = t[i]
+                    taken.add(t[i])
+                else:
+                    return False
+                
+        return True
+
+# time and space complexity
+# time complexity = O(n)
+# space complexity = O(n)
